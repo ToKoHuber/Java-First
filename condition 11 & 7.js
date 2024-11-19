3b46adaa800d81380c257a94100e271ac1de9427
24LP2879 24LP2879 <24LP2879@G304-CC0233.local>
2024-11-19 11:25:14 +0800

    Java

---
+++ condition 11 & 7.js
@@ -0,0 +1,15 @@
+let too = 66;
+
+if (too % 7 == 0) {
+  if (too % 11 == 0) {
+    console.log("7 bolon 11t huvaagdana");
+  } else {
+    console.log("7t huvaagdana 11t huvaagdahgvi");
+  }
+} else {
+  if (too % 11 == 0) {
+    console.log("7t huvaagdahgvi 11t huvaagdana");
+  } else {
+    console.log("11t bolon 7t huvaagdahgvi");
+  }
+}
---
+++ condition.js
@@ -0,0 +1,16 @@
+let dorom = true;
+let durem = 12;
+
+if (dorom == true) {
+  if (durem > 17) {
+    console.log("pass");
+  } else {
+    console.log("durmee dawt");
+  }
+} else {
+  if (durem < 17) {
+    console.log("doromoo dawt");
+  } else {
+    console.log("failed");
+  }
+}
