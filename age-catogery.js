dcaa81ce74f262efd3ab070f974aec34fd7b7b9d
24LP2879 24LP2879 <24LP2879@G304-CC0233.local>
2024-11-20 10:40:09 +0800

    loop

---
+++ age-catogery.js
@@ -0,0 +1,5 @@
+let nas = 20;
+
+if (o=<nas=<13) {
+    console.log("Child");}
+
---
+++ let .js
@@ -0,0 +1,11 @@
+let i = 11;
+
+// for (i = 0; i <= 11; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+for (i = 1; i <= 11; i = i + 2) {
+  console.log(i);
+}
