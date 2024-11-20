5a468f42fe4921747943a81b31ce2357b8e1bdb7
24LP2879 24LP2879 <24LP2879@G304-CC0233.local>
2024-11-20 08:38:31 +0800

    even odd 2.5

---
+++ even-odd-2.5.js
@@ -0,0 +1,15 @@
+let a = 12;
+
+if (a % 2 == 0) {
+  if (0 < a) {
+    console.log("Even and positive");
+  } else {
+    console.log("even and negative");
+  }
+} else {
+  if (0 < a) {
+    console.log("odd and positive");
+  } else {
+    console.log("odd and negative");
+  }
+}
